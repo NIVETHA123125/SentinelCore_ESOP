a870d98ce8b6d8607dd981376db78fa28851b785
Nivetha@06 <nivethavivekananthan1806@gmail.com>
2026-08-07 18:26:46 +0530

    split dashboard into seperate component

--- sentinelcore_frontend/frontend/src/api/assetApi.js
+++ sentinelcore_frontend/frontend/src/api/assetApi.js
@@ -1,14 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_BASE = "http://localhost:8080/api/assets";
+const API_BASE = 'http://localhost:8080/api/assets';
 
 export const getAllAssets = () => axios.get(API_BASE);
-
-export const getDashboardSummary = () =>
-    axios.get(`${API_BASE}/dashboard/summary`);
-
-export const createAsset = (asset) =>
-    axios.post(API_BASE, asset);
-
-export const deleteAsset = (id) =>
-    axios.delete(`${API_BASE}/${id}`);
+export const getDashboardSummary = () => axios.get(`${API_BASE}/dashboard/summary`);
+export const createAsset = (data) => axios.post(API_BASE, data);
+export const deleteAsset = (id) => axios.delete(`${API_BASE}/${id}`);
